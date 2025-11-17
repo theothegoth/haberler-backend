040afd41013bcc3536e4191a1637bfe28f98629f
theothegoth <huseyinguven24@hotmail.com>
2025-11-17 18:15:21 +0300

    Fix: Correct imports in email notification system

--- jobs/weeklyDigest.js
+++ jobs/weeklyDigest.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const pool = require('../config/database');
 const { sendWeeklyDigestEmail } = require('../services/emailService');
 const { getUsersWithWeeklyDigest } = require('../controllers/emailPreferencesController');
-const Logger = require('../utils/logger');
+const { Logger } = require('../utils/logger');
 
 const logger = new Logger('WEEKLY_DIGEST');
 
--- routes/testEmailRoutes.js
+++ routes/testEmailRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authenticate } = require('../middleware/authenticate');
+const { authenticate } = require('../middleware/auth');
 const { runManualDigest } = require('../jobs/weeklyDigest');
 const { sendNewFollowerEmail, sendNewCommentEmail, sendNewLikeEmail } = require('../services/emailService');
 const pool = require('../config/database');
